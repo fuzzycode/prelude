34b043c7faa14ac1a31dd25863e647cc547d1210
Björn Larsson <b.e.larsson@gmail.com>
2015-11-23 08:55:18 +0100

    Added visual settings

---
+++ personal/preload/bl-visuals.el
@@ -0,0 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; VISUAL CHANGES
+;;
+;; These are loaded before prelude loades anything
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;(setq prelude-theme 'color-theme-heroku)
+
+(setq initial-frame-alist
+      '(
+        (width . 120) ; character
+        (height . 48) ; lines
+        ))
