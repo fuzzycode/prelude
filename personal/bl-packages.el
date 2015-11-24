a4cc251c4fd6d866820f210218867e820fe3bf5d
Björn Larsson <b.e.larsson@gmail.com>
2015-11-24 14:07:58 +0100

    Added ace-jump-mode to list of packages.

--- personal/bl-packages.el
+++ personal/bl-packages.el
@@ -4,4 +4,4 @@
 ;; Configure standard packages to download
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(yasnippet))
+(prelude-require-packages '(yasnippet ace-jump-mode))
