f0f4af55e61426da01b307d5684a5e572c326024
Björn Larsson <b.e.larsson@gmail.com>
2015-11-29 21:48:31 +0100

    Turn on IDO mode by default. Updated some settings for ido mode.

--- personal/bl-ido.el
+++ personal/bl-ido.el
@@ -1,4 +1,4 @@
-;;; Package --- IDO settings
+;;; bl-ido.el --- IDO settings
 
 ;;; Commentary:
 
@@ -15,11 +15,16 @@
  ido-enable-last-directory-history t ; remember last used dirs
  ido-max-work-directory-list 30   ; should be enough
  ido-max-work-file-list      50   ; remember many
- ido-use-filename-at-point nil    ; don't use filename at point (annoying)
+ ido-use-filename-at-point 1    ; don't use filename at point (annoying)
  ido-use-url-at-point nil         ; don't use url at point (annoying)
- ido-enable-flex-matching nil     ; don't try to be too smart
+ ido-enable-flex-matching 1
  ido-max-prospects 8              ; don't spam my minibuffer
- ido-confirm-unique-completion t) ; wait for RET, even with unique completion
+ ido-confirm-unique-completion t  ; wait for RET, even with unique completion
+ ido-ignore-extensions t
+ completion-ignored-extensions
+ '("elc")) 
+
+(ido-mode 1)
 
 (provide 'bl-ido)
 ;;; bl-ido.el ends here
