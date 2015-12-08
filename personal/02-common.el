d3bfeff5885bcf80e02e9ca6e3c469f42c0dcb02
Björn Larsson <b.e.larsson@gmail.com>
2015-12-08 21:56:32 +0100

    Added a file for all common settings applying to all of emacs.

---
+++ personal/02-common.el
@@ -0,0 +1,14 @@
+;;; 02-common.el --- Common global Emacs settings
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defalias 'yes-or-no-p 'y-or-n-p) ;; Shorten the answers
+
+
+(setq require-final-newline t)           ;; end files with a newline
+
+(provide '02-common)
+;;; 02-common ends here
